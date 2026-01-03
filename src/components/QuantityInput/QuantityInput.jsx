cfbb46e8a48666dd101dbfca3265015f22decf15
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-01-03 17:30:08 -0600

    Added quanitity input component to select prod quantity that will go into the users cart

---
+++ src/components/QuantityInput/QuantityInput.jsx
@@ -0,0 +1,41 @@
+import QtyInputStyles from "./QuantityInput.module.css";
+
+function QuantityInput({ prodQuantity, setProdQuantity }) {
+  function addOne() {
+    setProdQuantity(prodQuantity + 1);
+  }
+
+  function subtractOne() {
+    if (prodQuantity > 1) return setProdQuantity(prodQuantity - 1);
+  }
+
+  function onChange(e) {
+    setProdQuantity(parseInt(e.target.value));
+  }
+
+  return (
+    <div className={QtyInputStyles.input}>
+      <button
+        className={`${QtyInputStyles.button} ${QtyInputStyles.left}`}
+        onClick={subtractOne}
+      >
+        -
+      </button>
+      <input
+        className={QtyInputStyles.numInput}
+        type="number"
+        min="1"
+        value={prodQuantity}
+        onChange={onChange}
+      ></input>
+      <button
+        className={`${QtyInputStyles.button} ${QtyInputStyles.right}`}
+        onClick={addOne}
+      >
+        +
+      </button>
+    </div>
+  );
+}
+
+export default QuantityInput;
