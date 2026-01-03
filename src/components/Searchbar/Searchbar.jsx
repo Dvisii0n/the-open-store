52abd0a0e19111f2f92e1378300465f6c0e9e0f4
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-01-02 21:36:46 -0600

    Added searchbar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,7 +1,17 @@
 import SearchbarStyles from "./Searchbar.module.css";
 function Searchbar() {
   return (
-    <input className={SearchbarStyles.searchbar} placeholder="Search"></input>
+    <div className={SearchbarStyles.searchbar}>
+      <label className={SearchbarStyles.label} htmlFor="searchInput">
+        Search
+      </label>
+      <input
+        className={SearchbarStyles.searchInput}
+        name="searchInput"
+        id="searchInput"
+      ></input>
+      <button className={SearchbarStyles.searchButton}>Go</button>
+    </div>
   );
 }
 export default Searchbar;
