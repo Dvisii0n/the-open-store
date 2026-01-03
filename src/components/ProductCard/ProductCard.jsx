d5e903a5887443742c15b091a9a11624e74ac57b
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-01-03 17:29:29 -0600

    Added quantity input component along with prod quantity state variable, also changed stytling

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,20 +1,28 @@
+import { useState } from "react";
+import QuantityInput from "../QuantityInput/QuantityInput";
 import ProductCardStyles from "./ProductCard.module.css";
 
 function ProductCard({ productInfo }) {
+  const [prodQuantity, setProdQuantity] = useState(1);
+
   return (
     <div className={ProductCardStyles.productCard}>
       <img
         className={ProductCardStyles.prodImg}
         src={productInfo.thumbnail}
         alt={`Image of ${productInfo.title}`}
+        loading="lazy"
       ></img>
       <p className={ProductCardStyles.title}>
         {productInfo.title.toLowerCase()}
       </p>
 
       <p className={ProductCardStyles.price}>${productInfo.price}</p>
-
-      <p className={ProductCardStyles.rating}>{productInfo.rating}</p>
+      <QuantityInput
+        prodQuantity={prodQuantity}
+        setProdQuantity={setProdQuantity}
+      />
+      <button className={ProductCardStyles.add}>Add to cart</button>
     </div>
   );
 }
