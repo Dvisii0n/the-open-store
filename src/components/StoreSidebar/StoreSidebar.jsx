6b3d440c50c07507153ff5b1d7282b23c72debe4
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-01-03 17:31:30 -0600

    Added default category for showing all products and implemented render products according to selected category

--- src/components/StoreSidebar/StoreSidebar.jsx
+++ src/components/StoreSidebar/StoreSidebar.jsx
@@ -1,7 +1,7 @@
 import { useFetchURL } from "../../scripts/hooks.js";
 import StoreSidebarStyles from "./StoreSidebar.module.css";
 
-function StoreSidebar() {
+function StoreSidebar({ setProductsURL, currentCategory }) {
   const url = "https://dummyjson.com/products/categories";
   const { data: categories, error } = useFetchURL(url);
 
@@ -9,16 +9,43 @@ function StoreSidebar() {
 
   return (
     <div className={StoreSidebarStyles.sidebar}>
+      <div className={StoreSidebarStyles.sidebarLabel}>Shop by category</div>
       {categories ? (
-        categories.map((category) => (
-          <p key={category.slug} className={StoreSidebarStyles.category}>
-            {category.name}
+        <>
+          {/* default all category */}
+          <p
+            onClick={() => setProductsURL(`https://dummyjson.com/products`)}
+            className={
+              currentCategory === "products"
+                ? `${StoreSidebarStyles.category} ${StoreSidebarStyles.active}`
+                : StoreSidebarStyles.category
+            }
+          >
+            All
           </p>
-        ))
+          {categories.map((category) => (
+            <p
+              key={category.slug}
+              onClick={() =>
+                setProductsURL(
+                  `https://dummyjson.com/products/category/${category.slug}`,
+                )
+              }
+              className={
+                currentCategory === category.slug
+                  ? `${StoreSidebarStyles.category} ${StoreSidebarStyles.active}`
+                  : StoreSidebarStyles.category
+              }
+            >
+              {category.name}
+            </p>
+          ))}
+        </>
       ) : (
         <p>Loading categories...</p>
       )}
     </div>
   );
 }
+
 export default StoreSidebar;
