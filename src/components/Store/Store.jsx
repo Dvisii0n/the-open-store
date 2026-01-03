c16e8e629f3c5f6b438e3d1c999e24130a874b06
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-01-02 21:37:53 -0600

    Added fetched products and categories rendering

--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -1,7 +1,32 @@
 import StoreStyles from "./Store.module.css";
+import StoreSidebar from "../StoreSidebar/StoreSidebar.jsx";
+import { useState } from "react";
+import { useFetchURL } from "../../scripts/hooks.js";
+import ProductCard from "../ProductCard/ProductCard.jsx";
 
 function Store() {
-  return <div className={StoreStyles.store}>Im da store</div>;
+  const [productsURL, setProductsURL] = useState(
+    "https://dummyjson.com/products",
+  );
+  const { data, error } = useFetchURL(productsURL);
+  const products = data ? data.products : null;
+
+  if (error) return <p>{error.message}</p>;
+
+  return (
+    <div className={StoreStyles.store}>
+      <StoreSidebar />
+      <div className={StoreStyles.products}>
+        {products ? (
+          products.map((prodInfo) => (
+            <ProductCard key={prodInfo.id} productInfo={prodInfo} />
+          ))
+        ) : (
+          <p>Loading products...</p>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Store;
