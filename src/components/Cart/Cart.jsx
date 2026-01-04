1402f452f3972a00a1f3123296673577a9184664
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-01-04 16:31:17 -0600

    Implemented cart page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,21 @@
+import { useOutletContext } from "react-router";
 import CartStyles from "./Cart.module.css";
+import CartItem from "../CartItem/CartItem";
 
 function Cart() {
-  return <div className={CartStyles.cart}>im da cart</div>;
+  const [cartItems] = useOutletContext();
+
+  const cartItemsArr = Object.values(cartItems);
+
+  if (!Object.keys(cartItems).length)
+    return <p className={CartStyles.empty}>Your cart is empty</p>;
+  return (
+    <div className={CartStyles.cart}>
+      {cartItemsArr.map((item) => (
+        <CartItem key={item.info.id} itemInfo={item.info} />
+      ))}
+    </div>
+  );
 }
 
 export default Cart;
