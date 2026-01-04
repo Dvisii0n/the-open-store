f5149848304026dde1c45713540b027fb93415fe
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-01-04 16:30:53 -0600

    Added cart items context

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,15 @@ import { Outlet } from "react-router";
 import Navbar from "../Navbar/Navbar.jsx";
 import AppStyles from "./App.module.css";
 import Footer from "../Footer/Footer.jsx";
+import { useState } from "react";
 
 function App() {
+  const [cartItems, setCartItems] = useState({});
+  console.log(cartItems);
   return (
     <div className={AppStyles.app}>
       <Navbar />
-      <Outlet />
+      <Outlet context={[cartItems, setCartItems]} />
       <Footer />
     </div>
   );
