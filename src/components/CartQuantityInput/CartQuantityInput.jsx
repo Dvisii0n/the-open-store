d6d929b9ac220a829e51edf4358d24e4b6cc30df
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-01-04 16:33:03 -0600

    Added quantity input component for item carts specifically

---
+++ src/components/CartQuantityInput/CartQuantityInput.jsx
@@ -0,0 +1,54 @@
+import CartQtyInputStyles from "./CartQuantityInput.module.css";
+
+//modified quantity input component because this one changes product quantity inside the cart
+function CartQuantityInput({
+  prodQuantity,
+  setProdQuantity,
+  updateCartQuantity,
+}) {
+  function addOne() {
+    const sum = prodQuantity + 1;
+    setProdQuantity(sum);
+    updateCartQuantity(sum);
+  }
+
+  function subtractOne() {
+    if (prodQuantity > 1) {
+      const diff = prodQuantity - 1;
+      setProdQuantity(diff);
+      updateCartQuantity(diff);
+    }
+  }
+
+  function onChange(e) {
+    const val = parseInt(e.target.value);
+    setProdQuantity(val);
+    updateCartQuantity(val);
+  }
+
+  return (
+    <div className={CartQtyInputStyles.input}>
+      <button
+        className={`${CartQtyInputStyles.button} ${CartQtyInputStyles.left}`}
+        onClick={subtractOne}
+      >
+        -
+      </button>
+      <input
+        className={CartQtyInputStyles.numInput}
+        type="number"
+        min="1"
+        value={prodQuantity}
+        onChange={onChange}
+      ></input>
+      <button
+        className={`${CartQtyInputStyles.button} ${CartQtyInputStyles.right}`}
+        onClick={addOne}
+      >
+        +
+      </button>
+    </div>
+  );
+}
+
+export default CartQuantityInput;
