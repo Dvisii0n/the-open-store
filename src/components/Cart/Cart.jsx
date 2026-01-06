ec6b72007914e79ebde675aecbcf57439314c9c9
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-01-05 19:55:37 -0600

    Added item count to cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,8 @@
 import { useOutletContext } from "react-router";
 import CartStyles from "./Cart.module.css";
 import CartItem from "../CartItem/CartItem";
+import Checkout from "../Checkout/Checkout";
+import { getItemCount } from "../../scripts/utils";
 
 function Cart() {
   const [cartItems] = useOutletContext();
@@ -11,9 +13,13 @@ function Cart() {
     return <p className={CartStyles.empty}>Your cart is empty</p>;
   return (
     <div className={CartStyles.cart}>
-      {cartItemsArr.map((item) => (
-        <CartItem key={item.info.id} itemInfo={item.info} />
-      ))}
+      <div className={CartStyles.cartList}>
+        <p>Total items: {getItemCount(cartItems)}</p>
+        {cartItemsArr.map((item) => (
+          <CartItem key={item.info.id} itemInfo={item.info} />
+        ))}
+      </div>
+      <Checkout />
     </div>
   );
 }
