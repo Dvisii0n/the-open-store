378868cb0436a02ec2258d09d1482f221d3e79e2
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-01-05 19:57:43 -0600

    Added item count to cart label

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,16 +2,24 @@ import NavbarStyles from "./Navbar.module.css";
 import Searchbar from "../Searchbar/Searchbar.jsx";
 import { Link } from "react-router";
 import Logo from "../Logo/Logo.jsx";
-function Navbar() {
+function Navbar({ itemCount }) {
   return (
     <nav className={NavbarStyles.navbar}>
       <Link to="/">
         <Logo></Logo>
       </Link>
       <Searchbar />
-      <Link to="store">Store</Link>
+      <Link to="store">
+        <p className={NavbarStyles.store}>Store</p>
+      </Link>
 
-      <Link to="cart">Cart</Link>
+      <Link to="cart">
+        <div className={NavbarStyles.cartWrapper}>
+          <p className={NavbarStyles.cart}>Cart </p>
+
+          <p className={NavbarStyles.count}>{itemCount}</p>
+        </div>
+      </Link>
     </nav>
   );
 }
