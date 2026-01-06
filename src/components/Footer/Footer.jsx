33092ef56541e54981dd8bc3429c0b3b08d998c9
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-01-05 19:57:17 -0600

    Added footer info

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,16 @@
 import FooterStyles from "./Footer.module.css";
-
+import { Link } from "react-router";
 function Footer() {
-  return <footer>Footer info here</footer>;
+  return (
+    <footer>
+      <div className={FooterStyles.list}>
+        The Odin Project 2026
+        <Link to="/">Home</Link>
+        <Link to="store">Store</Link>
+        <Link to="cart">Cart</Link>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
