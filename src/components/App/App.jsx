c36f9576a9b399c9bb5371908eddff6e1c4e742c
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-01-05 19:55:13 -0600

    Implemented cart item count

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,13 +3,16 @@ import Navbar from "../Navbar/Navbar.jsx";
 import AppStyles from "./App.module.css";
 import Footer from "../Footer/Footer.jsx";
 import { useState } from "react";
+import { getItemCount } from "../../scripts/utils.js";
 
 function App() {
   const [cartItems, setCartItems] = useState({});
-  console.log(cartItems);
+
+  const itemCount = getItemCount(cartItems);
+
   return (
     <div className={AppStyles.app}>
-      <Navbar />
+      <Navbar itemCount={itemCount} />
       <Outlet context={[cartItems, setCartItems]} />
       <Footer />
     </div>
